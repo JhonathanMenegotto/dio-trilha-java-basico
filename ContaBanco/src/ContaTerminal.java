ee087f07bfed3109a6aea2ce0ba9b93b11715e58
Jhonathan Menegotto <jhonathanmenegotto722@gmail.com>
2025-08-31 15:14:40 -0300

    Conta Banco com comentários de Código

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -4,12 +4,15 @@ public class ContaTerminal{
 
     public static void main(String[] args){
 
+        //Declaração de variáveis
         int numero;
         String agencia, nomeCliente;
         double saldo;
 
+        // Importar a classe Scanner;
         Scanner sc = new Scanner(System.in);
 
+        // Obter os valores pela classe Scanner
         System.out.print("Por favor digite o nome do cliente: ");
         nomeCliente = sc.nextLine();
         System.out.print("Por favor digite o número da agência: ");
@@ -19,9 +22,11 @@ public class ContaTerminal{
         System.out.print("Por favor digite o valor de depósito: ");
         saldo = sc.nextDouble();
 
+        // Apresentar na tela os valores obtidos e a conta criada.
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " +
                            agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
 
+        // Fechar a classe Scanner;
         sc.close();
 
     }
